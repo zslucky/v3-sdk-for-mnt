70a6d67183e4a33a8902fe6d20500c2e52736fc8
Lucky Zhou <zsney.lzhou@gmail.com>
2024-09-07 22:02:06 +0800

    chore: update pool init hash for mantle

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,9 @@ export const FACTORY_ADDRESS = '0xFd6D175Da8593bc695ff50464e3CD3124f2488ee'
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
 
-export const POOL_INIT_CODE_HASH = '0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54'
+// export const POOL_INIT_CODE_HASH = '0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54'
+export const POOL_INIT_CODE_HASH = '0xa598dd2fba360510c5a8f02f44423a4468e902df5857dbce3ca162a43a3a31ff'
+
 
 /**
  * The default factory enabled fee amounts, denominated in hundredths of bips.
